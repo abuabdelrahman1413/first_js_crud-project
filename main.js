ce4c0b774c617938fdd355818dec99a55bf890c2
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-08-26 20:20:57 +0200

    Refactor main.js for improved code organization and readability

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Variables
 let tasksList = [];
-tasksList = JSON.parse(localStorage.getItem("tasks"))
 let tasksListElement = document.getElementById('tasks_area');
 let addTaskButton = document.getElementById('add_btn');
 
+getFromLocalStorage();
 renderTasks();
 // Functions
 function renderTasks() {
@@ -25,12 +25,11 @@ function renderTasks() {
         <span class="material-symbols-outlined"> delete </span>
       </button>
   
-      ${
-        task.isDone
-          ? `      <button onclick="toggleTaskCompleation(${index})" class="circular_button create_btn cancel">
+      ${task.isDone
+        ? `      <button onclick="toggleTaskCompleation(${index})" class="circular_button create_btn cancel">
             <span class="material-symbols-outlined"> cancel </span>
           </button>`
-          : `      <button onclick="toggleTaskCompleation(${index})" class="circular_button create_btn check">
+        : `      <button onclick="toggleTaskCompleation(${index})" class="circular_button create_btn check">
             <span class="material-symbols-outlined"> check </span>
           </button>`
       }
@@ -54,15 +53,14 @@ function addTask() {
     let now = new Date();
     let taskDate =
       now.getDate() + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
-  
+
     let taskObject = {
       title: taskTitle,
       date: taskDate,
       isDone: false,
     };
     tasksList.push(taskObject);
-    let taskJson = JSON.stringify(tasksList);
-    localStorage.setItem('tasks', taskJson);
+    storeToLocalStorage();
     renderTasks();
   }
 }
@@ -71,8 +69,7 @@ function deleteTask(index) {
   let isConfirmed = confirm('هل تريد حذف المهمة؟');
   if (isConfirmed) {
     tasksList.splice(index, 1);
-    let taskJson = JSON.stringify(tasksList);
-    localStorage.setItem('tasks', taskJson);
+    storeToLocalStorage();
     renderTasks();
   }
 }
@@ -84,8 +81,7 @@ function updateTask(index) {
     now.getDate() + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
   tasksList[index].title = newTitle;
   tasksList[index].date = newDate;
-  let taskJson = JSON.stringify(tasksList);
-  localStorage.setItem('tasks', taskJson);
+  storeToLocalStorage();
 
   renderTasks();
 }
@@ -98,10 +94,20 @@ function toggleTaskCompleation(index) {
     renderTasks();
   } else {
     tasksList[index].isDone = true;
-    let taskJson = JSON.stringify(tasksList);
-    localStorage.setItem('tasks', taskJson);
+    storeToLocalStorage();
     renderTasks();
   }
+}
+
+function storeToLocalStorage() {
+  let taskJson = JSON.stringify(tasksList);
+  localStorage.setItem('tasks', taskJson);
+}
+
+function getFromLocalStorage() {
+let tasksFromJson = JSON.parse(localStorage.getItem("tasks"))
+tasksList = tasksFromJson ?? []
+
 }
 
 // Events
